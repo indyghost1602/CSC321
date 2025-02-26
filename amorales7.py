a6de8c0adda0fb0fce4cb912161b8422f53e9d85
Anthony Morales <anthonymo16@yahoo.com>
2025-02-26 02:09:44 +0000

    lab7 python

---
+++ amorales7.py
@@ -0,0 +1,26 @@
+#anthony morales
+#CSC321
+#Lab7
+
+x = 6
+y = 7.7
+z = "marvel"
+
+a = 10
+b = 8.5
+c = "image"
+
+if x > a:
+    print("variable x (6) is greater than variable a (10)")
+else: 
+    print("variable a (10) is greater than variable x (6)")
+
+if y > b:
+    print("variable y (7.7) is greater than variable b (8.5)")
+else:
+    print("variable b (8.5) is greater than variable y (7.7)")
+
+if z > c:
+    print("variable z ('marvel') is greater than variable c ('image')")
+else:
+    print("variable c ('image) is greater than variable z ('marvel')")
